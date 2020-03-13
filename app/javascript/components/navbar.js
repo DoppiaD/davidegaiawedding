ac78f1d284d7f7443acfacdbd15c0ac59923b19c
Davide <davidelora@outlook.com>
2020-03-13 12:31:29 +0000

    navbar transition javascript applies only to homepage

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,16 +1,20 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('#navbar-main');
   const navbarMobile = document.querySelector('#navbar-mobile');
-  if (navbar) {
+  const banner = document.querySelector('.home-banner');
+  if (navbar && banner) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-davide-white');
-        navbarMobile.classList.add('navbar-davide-white');
+        navbar.classList.add('navbar-davide-colorize');
+        navbarMobile.classList.add('navbar-davide-colorize');
       } else {
-        navbar.classList.remove('navbar-davide-white');
-        navbarMobile.classList.remove('navbar-davide-white');
+        navbar.classList.remove('navbar-davide-colorize');
+        navbarMobile.classList.remove('navbar-davide-colorize');
       }
     });
+  } else {
+    navbar.classList.add('navbar-davide-colorize');
+    navbarMobile.classList.add('navbar-davide-colorize');
   }
 };
 
