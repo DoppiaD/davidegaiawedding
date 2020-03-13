a71b04af310aea4efb420363122457b2661b0018
Davide <davidelora@outlook.com>
2020-03-13 10:00:37 +0000

    devise config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :registerable, :confirmable, :recoverable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :rememberable,
+          :validatable, :trackable
 end
