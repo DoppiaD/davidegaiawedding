3a798bb40629999f38cb9b9c7c1a59c61f96d58a
Davide <davidelora@outlook.com>
2020-03-13 19:26:59 +0000

    refactored navbar html, css, js

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,20 +1,16 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('#navbar-main');
-  const navbarMobile = document.querySelector('#navbar-mobile');
+  const navbar = document.querySelector('.navbar-davide');
   const banner = document.querySelector('.home-banner');
   if (navbar && banner) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-davide-colorize');
-        navbarMobile.classList.add('navbar-davide-colorize');
       } else {
         navbar.classList.remove('navbar-davide-colorize');
-        navbarMobile.classList.remove('navbar-davide-colorize');
       }
     });
   } else {
     navbar.classList.add('navbar-davide-colorize');
-    navbarMobile.classList.add('navbar-davide-colorize');
   }
 };
 
