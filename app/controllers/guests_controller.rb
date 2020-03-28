a35103f4cdba190e9213f550c2ce41b672d4c909
Davide <davidelora@outlook.com>
2020-03-28 19:46:28 +0000

    rails g Guests controller + routes

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,19 @@
+class GuestsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def udpate
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
-  # get "/:page" => "pages#show"
   get '/logistics', to: 'pages#logistics'
   get '/tourist-info', to: 'pages#touristinfo'
+  resources :guests, :except => [:destroy]
 end
