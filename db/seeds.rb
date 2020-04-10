54fbd672fdbf8b714171cd41e9f4138dce097dec
Davide <davidelora@outlook.com>
2020-04-10 22:57:09 +0100

    +1 image logic - index View

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ puts "Creating seed of Guests"
 Guest.create!(user: u, name: "Bilbo", participate: true, last_name: "Baggings", allergies: "Goblins", shuttle_to: true, shuttle_from: false)
 Guest.create!(user: u, name: "Frodo", participate: false, last_name: "Baggings", allergies: "Uruk Hai", shuttle_to: true, shuttle_from: true, child: true)
 Guest.create!(user: u, name: "Frodo2", participate: false, last_name: "Baggings", allergies: "Uruk Hai", shuttle_to: true, shuttle_from: true, child: true)
-Guest.create!(user: u, name: "Frodo1", participate: true, last_name: "Baggings", allergies: "Uruk Hai", shuttle_to: true, shuttle_from: true, child: true)
+Guest.new(user: u).save(validate: false)
 
 Guest.create!(user: c, name: "Samwise", participate: true, last_name: "Gamgee", allergies: "Brains", shuttle_to: false, shuttle_from: true)
 
