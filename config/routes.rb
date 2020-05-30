afa35ec2c31f329d0d8d7ecd3680b10a3e66bacf
Davide <davidelora@outlook.com>
2020-05-30 17:34:24 +0100

    initial i18n basic setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  root to: 'pages#home'
-  get '/logistics', to: 'pages#logistics'
-  get '/tourist-info', to: 'pages#touristinfo'
 
-  resources :guests, only: [:index, :edit, :update] do
-    member do
-      patch :participate
+  scope '(:locale)', locale: /it/ do
+    root to: 'pages#home'
+    get '/logistics', to: 'pages#logistics'
+    get '/tourist-info', to: 'pages#touristinfo'
+
+    resources :guests, only: [:index, :edit, :update] do
+      member do
+        patch :participate
+      end
+      # patch '/guests/:id/participate', to: 'guests#participate'
     end
-    # patch '/guests/:id/participate', to: 'guests#participate'
+    resources :registries, only: [:index]
+    resources :user_registries, only: [:create, :destroy]
   end
-  resources :registries, only: [:index]
-  resources :user_registries, only: [:create, :destroy]
 end
